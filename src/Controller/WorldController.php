31de234f0780dd69232b5cc573483e317127ce66
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-06-20 13:58:12 +0300

    Add service with string argument

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -2,28 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Manager\UserManager;
-use App\Service\UserBuilderService;
+use App\Service\GreeterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class WorldController extends AbstractController
 {
-    public function __construct(private readonly UserManager        $userManager,
-                                private readonly UserBuilderService $userBuilderService
-    )
+    public function __construct(private readonly GreeterService $greeterService)
     {
     }
 
     public function hello(): Response
     {
-        $user = $this->userBuilderService->createUserWithTweets(
-            'Charles Dickens',
-            ['Oliver Twist', 'The Christmas Carol']
-        );
-        $userData = $this->userManager->findUserWithTweetsWithQueryBuilder($user->getId());
-
-        return $this->json($userData->toArray());
+        return new Response("<html><body>{$this->greeterService->greet('world')}</body></html>");
     }
 }
