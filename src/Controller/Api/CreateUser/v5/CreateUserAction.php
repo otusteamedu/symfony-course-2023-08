c8a254dc252792dc3dada2e2c060938609fbb8bf
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-09-05 15:28:06 +0300

    Add synchronous command

--- src/Controller/Api/CreateUser/v5/CreateUserAction.php
+++ src/Controller/Api/CreateUser/v5/CreateUserAction.php
@@ -5,18 +5,20 @@ namespace App\Controller\Api\CreateUser\v5;
 use App\Controller\Api\CreateUser\v5\Input\CreateUserDTO;
 use App\Controller\Api\CreateUser\v5\Output\UserIsCreatedDTO;
 use App\Controller\Common\ErrorResponseTrait;
+use App\Domain\Command\CreateUser\CreateUserCommand;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use OpenApi\Attributes as OA;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class CreateUserAction extends AbstractFOSRestController
 {
     use ErrorResponseTrait;
 
-    public function __construct(private readonly CreateUserManagerInterface $saveUserManager)
+    public function __construct(private readonly MessageBusInterface $messageBus)
     {
     }
 
@@ -38,9 +40,8 @@ class CreateUserAction extends AbstractFOSRestController
     )]
     public function saveUserAction(#[MapRequestPayload] CreateUserDTO $request): Response
     {
-        $user = $this->saveUserManager->saveUser($request);
-        [$data, $code] = ($user->id === null) ? [['success' => false], 400] : [['user' => $user], 200];
+        $this->messageBus->dispatch(CreateUserCommand::createFromRequest($request));
 
-        return $this->handleView($this->view($data, $code));
+        return $this->handleView($this->view(['success' => true]));
     }
 }
