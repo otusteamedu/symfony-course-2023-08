otusteamedu/symfony-course-2023-05

Add UserController

src/Controller/Api/v1/UserController.php
@@ -1,0 +1,63 @@
+<?php
+
+namespace App\Controller\Api\v1;
+
+use App\Manager\UserManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/api/v1/user')]
+class UserController extends AbstractController
+{
+    private const DEFAULT_PAGE = 0;
+    private const DEFAULT_PER_PAGE = 20;
+
+    public function __construct(private readonly UserManager $userManager)
+    {
+    }
+
+    #[Route(path: '', methods: ['POST'])]
+    public function saveUserAction(Request $request): Response
+    {
+        $login = $request->request->get('login');
+        $userId = $this->userManager->saveUser($login);
+        [$data, $code] = $userId === null ?
+            [['success' => false], Response::HTTP_BAD_REQUEST] :
+            [['success' => true, 'userId' => $userId], Response::HTTP_OK];
+
+        return new JsonResponse($data, $code);
+    }
+
+    #[Route(path: '', methods: ['GET'])]
+    public function getUsersAction(Request $request): Response
+    {
+        $perPage = $request->query->get('perPage');
+        $page = $request->query->get('page');
+        $users = $this->userManager->getUsers($page ?? self::DEFAULT_PAGE, $perPage ?? self::DEFAULT_PER_PAGE);
+        $code = empty($users) ? Response::HTTP_NO_CONTENT : Response::HTTP_OK;
+
+        return new JsonResponse(['users' => $users], $code);
+    }
+
+    #[Route(path: '', methods: ['DELETE'])]
+    public function deleteUserAction(Request $request): Response
+    {
+        $userId = $request->query->get('userId');
+        $result = $this->userManager->deleteUser($userId);
+
+        return new JsonResponse(['success' => $result], $result ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route(path: '', methods: ['PATCH'])]
+    public function updateUserAction(Request $request): Response
+    {
+        $userId = $request->request->get('userId');
+        $login = $request->request->get('login');
+        $result = $this->userManager->updateUser($userId, $login);
+
+        return new JsonResponse(['success' => $result], $result ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
+    }
+}

otusteamedu/symfony-course-2023-08

src/Controller/Api/v1/UserController.php
@@ -1,0 +1,63 @@
+<?php
+
+namespace App\Controller\Api\v1;
+
+use App\Manager\UserManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/api/v1/user')]
+class UserController extends AbstractController
+{
+    private const DEFAULT_PAGE = 0;
+    private const DEFAULT_PER_PAGE = 20;
+
+    public function __construct(private readonly UserManager $userManager)
+    {
+    }
+
+    #[Route(path: '', methods: ['POST'])]
+    public function saveUserAction(Request $request): Response
+    {
+        $login = $request->request->get('login');
+        $userId = $this->userManager->create($login);
+        [$data, $code] = $userId === null ?
+            [['success' => false], Response::HTTP_BAD_REQUEST] :
+            [['success' => true, 'userId' => $userId], Response::HTTP_OK];
+
+        return new JsonResponse($data, $code);
+    }
+
+    #[Route(path: '', methods: ['GET'])]
+    public function getUsersAction(Request $request): Response
+    {
+        $perPage = $request->query->get('perPage');
+        $page = $request->query->get('page');
+        $users = $this->userManager->getUsers($page ?? self::DEFAULT_PAGE, $perPage ?? self::DEFAULT_PER_PAGE);
+        $code = empty($users) ? Response::HTTP_NO_CONTENT : Response::HTTP_OK;
+
+        return new JsonResponse(['users' => $users], $code);
+    }
+
+    #[Route(path: '', methods: ['DELETE'])]
+    public function deleteUserAction(Request $request): Response
+    {
+        $userId = $request->query->get('userId');
+        $result = $this->userManager->deleteUser($userId);
+
+        return new JsonResponse(['success' => $result], $result ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route(path: '', methods: ['PATCH'])]
+    public function updateUserAction(Request $request): Response
+    {
+        $userId = $request->request->get('userId');
+        $login = $request->request->get('login');
+        $result = $this->userManager->updateUserLogin($userId, $login);
+
+        return new JsonResponse(['success' => $result], $result ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
+    }
+}