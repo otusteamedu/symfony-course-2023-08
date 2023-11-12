76efada7e6ecc8292c0eb587c656eb41ab2c77b5
AlexYasakov <chiter814@gmail.com>
2023-11-12 20:18:48 +0300

    Add open api docs for create user

--- src/Controller/Api/GetFeed/v1/Controller.php
+++ src/Controller/Api/GetFeed/v1/Controller.php
@@ -22,9 +22,24 @@ class Controller extends AbstractFOSRestController
     #[Route(path: '/api/v1/get-feed', methods: ['GET'])]
     #[QueryParam(name: 'userId', requirements: '\d+')]
     #[QueryParam(name: 'count', requirements: '\d+', nullable: true)]
-    #[OA\Tag(name: 'Лента')]
-    #[OA\Parameter(name: 'userId', description: 'ID пользователя', in: 'query', example: '135')]
-    #[OA\Parameter(name: 'count', description: 'Количество на странице', in: 'query', example: '1')]
+    #[OA\Get(
+        operationId: 'getFeed',
+        tags: ['Лента'],
+        parameters: [
+            new OA\Parameter(
+                name: 'userId',
+                description: 'ID пользователя',
+                in: 'query',
+                example: '135',
+            ),
+            new OA\Parameter(
+                name: 'count',
+                description: 'Количество на странице',
+                in: 'query',
+                example: '1',
+            ),
+        ]
+    )]
     public function getFeedAction(int $userId, ?int $count = null): View
     {
         $count = $count ?? self::DEFAULT_FEED_SIZE;
