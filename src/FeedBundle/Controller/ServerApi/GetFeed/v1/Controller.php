9b4b3d5e3e4fe1d4e8d9ba057935cb27146200cb
AlexYasakov <chiter814@gmail.com>
2023-11-19 19:41:40 +0300

    Controller for get feed in bundle

--- src/FeedBundle/Controller/ServerApi/GetFeed/v1/Controller.php
+++ src/FeedBundle/Controller/ServerApi/GetFeed/v1/Controller.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Controller\Api\GetFeed\v1;
+namespace FeedBundle\Controller\ServerApi\GetFeed\v1;
 
-use App\Facade\FeedFacade;
+use FeedBundle\Service\FeedService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
@@ -12,17 +12,17 @@ class Controller extends AbstractFOSRestController
     /** @var int */
     private const DEFAULT_FEED_SIZE = 20;
 
-    public function __construct(private readonly FeedFacade $feedFacade)
+    public function __construct(private readonly FeedService $feedService)
     {
     }
 
-    #[Rest\Get('/api/v1/get-feed')]
+    #[Rest\Get('/v1/get-feed')]
     #[Rest\QueryParam(name: 'userId', requirements: '\d+')]
     #[Rest\QueryParam(name: 'count', requirements: '\d+', nullable: true)]
     public function getFeedAction(int $userId, ?int $count = null): View
     {
         $count = $count ?? self::DEFAULT_FEED_SIZE;
-        $tweets = $this->feedFacade->getFeed($userId, $count);
+        $tweets = $this->feedService->getFeed($userId, $count);
         $code = empty($tweets) ? 204 : 200;
 
         return View::create(['tweets' => $tweets], $code);
